4c7486112ed0633e532ec13088cc703f5430c70a
Wajih Bhs <wajih.bhs@doinsport.com>
2025-09-12 17:58:39 +0200

    plateform: create project

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import VitalApp from "./components/VitalApp.vue";
+</script>
+
+<template>
+  <vital-app msg="Vital APP" />
+</template>
---
+++ src/components/VitalApp.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+defineProps<{ msg: string }>();
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <p class="welcome">Welcome to my Nurea Vital App</p>
+</template>
+
+<style lang="scss" scoped>
+.welcome {
+  color: #c52525;
+}
+</style>
